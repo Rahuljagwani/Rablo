2aa0f49644da16fc3b8f7fad910e71e5647607aa
Rahul <rahuljagwani1012@gmail.com>
2022-12-22 15:43:57 +0530

    sm

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import React  from "react";
 import Details from './Details';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
  
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Mform />} />
         <Route exact path="/details" 
         element={<Details authorized={false} />}
           />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
